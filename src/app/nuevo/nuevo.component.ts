4d7ac770642d8f861911d1f8a9e1d9d42560c35b
unknown <c31051@utp.edu.pe>
2025-06-16 01:07:24 -0500

    second commit

--- src/app/nuevo/nuevo.component.ts
+++ src/app/nuevo/nuevo.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-nuevo',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './nuevo.component.html',
   styleUrl: './nuevo.component.css'
 })
 export class NuevoComponent {
+  public txentrada:FormControl = new FormControl();
+  
+  public alertaNombre():void {
+    alert(this.txentrada.value)
+  }
 
 }
